274e3da101afa83335d1b27090819792495ed3d7
whiteof <victor@Viktars-Mac-mini.local>
2016-06-21 07:30:33 -0400

    Layout - header.

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,7 +1,37 @@
 jQuery(function($){
-    $(document).ready(function(){
+    $(window).load(function() {
+        function resizeItems() {
+            
+            //match wcatalog items' height
+            var wcat_max_height = 0;
+            $('.wcatalog-category').removeAttr('style');
+            $('.wcatalog-category').each(function(){
+                if($(this).height() > wcat_max_height) wcat_max_height = $(this).height();
+            });
+            $('.wcatalog-category').height(wcat_max_height);
+
+            //match wcatalog items' height
+            var wcat_max_height = 0;
+            $('.wcatalog-product').removeAttr('style');
+            $('.wcatalog-product').each(function(){
+                if($(this).height() > wcat_max_height) wcat_max_height = $(this).height();
+            });
+            $('.wcatalog-product').height(wcat_max_height);
+            
+        }
+        resizeItems();
+        $(window).resize(function() {
+            resizeItems();
+        }).trigger("resize");
         
-        dragula([document.getElementById('editor-samples'), document.getElementById('editor-storyboard')]);
-        
+        $('.visible-xs button').click(function(){
+            if ($(this).hasClass('expanded')) {
+                $(this).removeClass('expanded');
+                $(this).html('&rarr; ÐšÐ°Ñ‚Ð°Ð»Ð¾Ð³');
+            }else {
+                $(this).addClass('expanded');
+                $(this).html('&larr; ÐšÐ°Ñ‚Ð°Ð»Ð¾Ð³');
+            }
+        });
     });
 });
---
+++ public/js/offcanvas.js
@@ -0,0 +1,7 @@
+jQuery(function($){
+    $(document).ready(function () {
+        $('[data-toggle="offcanvas"]').click(function () {
+            $('.row-offcanvas').toggleClass('active')
+        });
+    });
+});
